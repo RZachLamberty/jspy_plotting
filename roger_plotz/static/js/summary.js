e26681f6e273eaef4e9c9f19de3e6d2a53b8d3e2
r.zach.lamberty <r.zach.lamberty@gmail.com>
2016-06-08 10:09:50 -0400

    markdown rendering: adding code highlighting

--- roger_plotz/static/js/summary.js
+++ roger_plotz/static/js/summary.js
@@ -1,6 +1,21 @@
 function render_summary_markdown(mdFileName, divid) {
+    var md = new Remarkable({
+        langPrefix: 'hljs ',
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, str).value;
+                } catch (err) {}
+            }
+
+            try {
+                return hljs.highlightAuto(str).value;
+            } catch (err) {}
+
+            return '';
+        }
+    });
     jQuery.get(mdFileName, function(data) {
-        var md = new Remarkable();
         $(divid).html(md.render(data));
     });
 }
